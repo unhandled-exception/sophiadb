2674b72c627da1788c84de92ae22bfa0b5c3d860
Oleg Volchkov <oleg@volchkov.net>
2022-08-27 00:51:45 +0300

    Добавил интерфейсы в pkg/scan, вынес итераторы в функции

---
+++ pkg/scan/iterators.go
@@ -0,0 +1,85 @@
+package scan
+
+import "github.com/unhandled-exception/sophiadb/pkg/records"
+
+func scanForEach(ts Scan, call func() (bool, error)) error {
+	if err := ts.BeforeFirst(); err != nil {
+		return err
+	}
+
+	for {
+		ok, err := ts.Next()
+		if !ok {
+			if err != nil {
+				return err
+			}
+
+			break
+		}
+
+		stop, err := call()
+		if err != nil {
+			return err
+		}
+
+		if stop {
+			break
+		}
+	}
+
+	return nil
+}
+
+func scanForEachField(ts Scan, call func(name string, fieldType records.FieldType) (bool, error)) error {
+	for _, name := range ts.Layout().Schema.Fields() {
+		fieldType := ts.Layout().Schema.Type(name)
+
+		stop, err := call(name, fieldType)
+		if err != nil {
+			return err
+		}
+
+		if stop {
+			break
+		}
+	}
+
+	return nil
+}
+
+func scanForEachValue(ts Scan, call func(name string, fieldType records.FieldType, value interface{}) (bool, error)) error {
+	for _, name := range ts.Layout().Schema.Fields() {
+		fieldType := ts.Layout().Schema.Type(name)
+
+		var (
+			value interface{}
+			err   error
+		)
+
+		switch fieldType {
+		case records.Int64Field:
+			value, err = ts.GetInt64(name)
+		case records.Int8Field:
+			value, err = ts.GetInt8(name)
+		case records.StringField:
+			value, err = ts.GetString(name)
+		default:
+			err = ErrUnknownFieldType
+		}
+
+		if err != nil {
+			return err
+		}
+
+		stop, err := call(name, fieldType, value)
+		if err != nil {
+			return err
+		}
+
+		if stop {
+			break
+		}
+	}
+
+	return nil
+}
