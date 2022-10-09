61f0526e67196244dd7013995e0d0242e888cb7e
Oleg Volchkov <oleg@volchkov.net>
2022-10-09 21:26:12 +0300

    Парсер для update #43

--- pkg/parse/parser.go
+++ pkg/parse/parser.go
@@ -30,6 +30,7 @@ func NewSQLParser() SQLParser {
 			StmtQuery:       func(q string) (Statement, error) { return NewSQLSelectStatement(q) },
 			StmtInsert:      func(q string) (Statement, error) { return NewSQLInsertStatement(q) },
 			StmtDelete:      func(q string) (Statement, error) { return NewSQLDeleteStatement(q) },
+			StmtUpdate:      func(q string) (Statement, error) { return NewSQLUpdateStatement(q) },
 			StmtCreateIndex: func(q string) (Statement, error) { return NewSQLCreateIndexStatement(q) },
 			StmtCreateView:  func(q string) (Statement, error) { return NewSQLCreateViewStatement(q) },
 		},
