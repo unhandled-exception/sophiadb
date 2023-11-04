c5a8c1a4f4b92346835976333190bcdcfbf8e36a
Oleg Volchkov <oleg@volchkov.net>
2023-11-04 12:03:29 +0300

    Добавил тесты для indexes.BTreeLeaf #76

--- internal/pkg/indexes/btree_leaf_test.go
+++ internal/pkg/indexes/btree_leaf_test.go
@@ -42,7 +42,7 @@ func (ts *BTreeLeafTestSuite) newSUT(layout records.Layout, searchkey scan.Const
 	btp, err := indexes.NewBTreePage(trx, block, layout)
 	require.NoError(t, err)
 
-	require.NoError(t, btp.FormatBlock(block, 0))
+	require.NoError(t, btp.FormatBlock(block, -1))
 
 	ts.fillTestPage(btp, data)
 
@@ -60,12 +60,13 @@ type btreeLeafTestsPageData struct {
 
 func (ts *BTreeLeafTestSuite) fillTestPage(page *indexes.BTreePage, data []btreeLeafTestsPageData) {
 	t := ts.T()
-	total := 0
+	slot := -1
 
 	for _, d := range data {
 		for i := 0; i < d.Count; i++ {
+			slot += 1
 			require.NoError(t, page.InsertLeaf(
-				types.SlotID(total+i),
+				types.SlotID(slot),
 				d.SearchKey,
 				types.RID{
 					BlockNumber: d.FirstRID.BlockNumber + types.BlockID(i),
@@ -73,8 +74,6 @@ func (ts *BTreeLeafTestSuite) fillTestPage(page *indexes.BTreePage, data []btree
 				},
 			))
 		}
-
-		total += d.Count
 	}
 }
 
@@ -129,6 +128,7 @@ func (ts *BTreeLeafTestSuite) TestInsertWithoutOverflow() {
 	}()
 
 	for i := 0; i < searchKeyCount; i++ {
+		require.NoError(t, sut.ResetSlot())
 		de, err := sut.Insert(types.RID{
 			BlockNumber: types.BlockID(int(firstRID.BlockNumber) + i),
 			Slot:        types.SlotID(int(firstRID.Slot) + i),
@@ -151,8 +151,8 @@ func (ts *BTreeLeafTestSuite) TestInsertWithoutOverflow() {
 		rid, err1 := sut.RID()
 		require.NoError(t, err1)
 		assert.Equalf(t, types.RID{
-			BlockNumber: types.BlockID(int(firstRID.BlockNumber) + i),
-			Slot:        types.SlotID(int(firstRID.Slot) + i),
+			BlockNumber: types.BlockID(int(firstRID.BlockNumber) + searchKeyCount - i - 1),
+			Slot:        types.SlotID(int(firstRID.Slot) + searchKeyCount - i - 1),
 		}, rid, "i = %d", i)
 		assert.EqualValues(t, testPageData[0].Count+i, sut.CurrentSlot())
 	}
@@ -162,7 +162,7 @@ func (ts *BTreeLeafTestSuite) TestInsertWithoutOverflow() {
 	assert.False(t, ok)
 }
 
-func (ts *BTreeLeafTestSuite) TestSplitPageOnInsertToPageWithMultipleValuesAndSplitPage() {
+func (ts *BTreeLeafTestSuite) TestSplitPageOnInsertToPageWithMultipleValues() {
 	t := ts.T()
 
 	layout := indexes.NewBTreeLeafPageLayout(records.Int64Field, 0)
@@ -180,7 +180,15 @@ func (ts *BTreeLeafTestSuite) TestSplitPageOnInsertToPageWithMultipleValuesAndSp
 	testPageData := []btreeLeafTestsPageData{
 		{
 			SearchKey: scan.NewInt64Constant(searchVal - 13),
-			Count:     int(maxRecs / 2),
+			Count:     int(maxRecs / 4),
+			FirstRID: types.RID{
+				BlockNumber: 235,
+				Slot:        350,
+			},
+		},
+		{
+			SearchKey: scan.NewInt64Constant(searchVal),
+			Count:     int((maxRecs/2 - maxRecs/4)),
 			FirstRID: types.RID{
 				BlockNumber: 235,
 				Slot:        350,
@@ -188,7 +196,7 @@ func (ts *BTreeLeafTestSuite) TestSplitPageOnInsertToPageWithMultipleValuesAndSp
 		},
 		{
 			SearchKey: scan.NewInt64Constant(searchVal + 13),
-			Count:     int((maxRecs - maxRecs/2) - 1),
+			Count:     int((maxRecs - maxRecs/2) - 2),
 			FirstRID: types.RID{
 				BlockNumber: 335,
 				Slot:        150,
@@ -202,12 +210,16 @@ func (ts *BTreeLeafTestSuite) TestSplitPageOnInsertToPageWithMultipleValuesAndSp
 		require.NoError(t, fm.Close())
 	}()
 
+	require.NoError(t, sut.ResetSlot())
+
 	de, err := sut.Insert(types.RID{
 		BlockNumber: types.BlockID(int(firstRID.BlockNumber) + 1),
 		Slot:        types.SlotID(int(firstRID.Slot) + 1),
 	})
 	assert.NoError(t, err)
 	assert.Nil(t, de)
+
+	require.NoError(t, sut.ResetSlot())
 
 	de, err = sut.Insert(types.RID{
 		BlockNumber: types.BlockID(int(firstRID.BlockNumber) + 2),
@@ -228,4 +240,255 @@ func (ts *BTreeLeafTestSuite) TestSplitPageOnInsertToPageWithMultipleValuesAndSp
 	ok, err := sut.Next()
 	assert.NoError(t, err)
 	assert.False(t, ok)
+}
+
+func (ts *BTreeLeafTestSuite) TestSplitPageOnInsertToPageWithKeyGreaterMedian() {
+	t := ts.T()
+
+	layout := indexes.NewBTreeLeafPageLayout(records.Int64Field, 0)
+
+	searchVal := int64(33)
+	searchKey := scan.NewInt64Constant(searchVal)
+
+	firstRID := types.RID{
+		BlockNumber: 145,
+		Slot:        0,
+	}
+
+	maxRecs := (defaultTestBlockSize - leafPageDataOffset) / layout.SlotSize
+
+	testPageData := []btreeLeafTestsPageData{
+		{
+			SearchKey: scan.NewInt64Constant(searchVal - 13),
+			Count:     int(maxRecs * 3 / 5),
+			FirstRID: types.RID{
+				BlockNumber: 235,
+				Slot:        350,
+			},
+		},
+		{
+			SearchKey: scan.NewInt64Constant(searchVal),
+			Count:     int(maxRecs - maxRecs*3/5 - 2),
+			FirstRID: types.RID{
+				BlockNumber: 235,
+				Slot:        350,
+			},
+		},
+	}
+
+	sut, page, _, fm := ts.newSUT(layout, searchKey, testPageData)
+	defer sut.Close()
+	defer func() {
+		require.NoError(t, fm.Close())
+	}()
+
+	require.NoError(t, sut.ResetSlot())
+
+	de, err := sut.Insert(types.RID{
+		BlockNumber: types.BlockID(int(firstRID.BlockNumber) + 1),
+		Slot:        types.SlotID(int(firstRID.Slot) + 1),
+	})
+	assert.NoError(t, err)
+	assert.Nil(t, de)
+
+	require.NoError(t, sut.ResetSlot())
+
+	de, err = sut.Insert(types.RID{
+		BlockNumber: types.BlockID(int(firstRID.BlockNumber) + 2),
+		Slot:        types.SlotID(int(firstRID.Slot) + 2),
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, &indexes.BTreeDirEntry{
+		BlockNumber: 1,
+		Dataval:     searchKey,
+	}, de)
+
+	recs, err := page.GetRecords()
+	require.NoError(t, err)
+	require.EqualValues(t, testPageData[0].Count, recs)
+
+	require.NoError(t, sut.ResetSlot())
+
+	ok, err := sut.Next()
+	assert.NoError(t, err)
+	assert.False(t, ok)
+}
+
+func (ts *BTreeLeafTestSuite) TestSplitPageOnInsertToPageWithSingleValue() {
+	t := ts.T()
+
+	layout := indexes.NewBTreeLeafPageLayout(records.Int64Field, 0)
+
+	searchVal := int64(33)
+	searchKey := scan.NewInt64Constant(searchVal)
+
+	firstRID := types.RID{
+		BlockNumber: 145,
+		Slot:        0,
+	}
+
+	maxRecs := (defaultTestBlockSize - leafPageDataOffset) / layout.SlotSize
+
+	testPageData := []btreeLeafTestsPageData{
+		{
+			SearchKey: scan.NewInt64Constant(searchVal),
+			Count:     int(maxRecs) - 1,
+			FirstRID:  firstRID,
+		},
+	}
+
+	sut, page, _, fm := ts.newSUT(layout, searchKey, testPageData)
+	defer sut.Close()
+	defer func() {
+		require.NoError(t, fm.Close())
+	}()
+
+	require.NoError(t, sut.ResetSlot())
+
+	de, err := sut.Insert(types.RID{
+		BlockNumber: types.BlockID(int(firstRID.BlockNumber) + 2),
+		Slot:        types.SlotID(int(firstRID.Slot) + 2),
+	})
+	assert.NoError(t, err)
+	assert.Nil(t, de)
+
+	recs, err := page.GetRecords()
+	require.NoError(t, err)
+	require.EqualValues(t, 1, recs)
+
+	require.NoError(t, sut.ResetSlot())
+
+	for i := 0; i < int(maxRecs); i++ {
+		ok, err := sut.Next()
+		assert.NoError(t, err)
+		assert.True(t, ok)
+	}
+}
+
+func (ts *BTreeLeafTestSuite) TestSplitPageOnInsertToPageWithOverflowBlock() {
+	t := ts.T()
+
+	layout := indexes.NewBTreeLeafPageLayout(records.Int64Field, 0)
+
+	searchVal := int64(33)
+	searchKey := scan.NewInt64Constant(searchVal)
+
+	firstRID := types.RID{
+		BlockNumber: 145,
+		Slot:        0,
+	}
+
+	maxRecs := (defaultTestBlockSize - leafPageDataOffset) / layout.SlotSize
+
+	testPageData := []btreeLeafTestsPageData{
+		{
+			SearchKey: scan.NewInt64Constant(searchVal + 13),
+			Count:     int(maxRecs),
+			FirstRID: types.RID{
+				BlockNumber: 335,
+				Slot:        150,
+			},
+		},
+	}
+
+	sut, page, _, fm := ts.newSUT(layout, searchKey, testPageData)
+	defer sut.Close()
+	defer func() {
+		require.NoError(t, fm.Close())
+	}()
+
+	require.NoError(t, page.SetFlag(100))
+
+	require.NoError(t, sut.ResetSlot())
+
+	de, err := sut.Insert(types.RID{
+		BlockNumber: types.BlockID(int(firstRID.BlockNumber) + 1),
+		Slot:        types.SlotID(int(firstRID.Slot) + 1),
+	})
+	assert.NoError(t, err)
+	assert.Equal(t,
+		&indexes.BTreeDirEntry{
+			BlockNumber: 1,
+			Dataval:     searchKey,
+		},
+		de,
+	)
+
+	recs, err := page.GetRecords()
+	require.NoError(t, err)
+	require.EqualValues(t, 1, recs)
+
+	require.NoError(t, sut.ResetSlot())
+
+	ok, err := sut.Next()
+	assert.NoError(t, err)
+	assert.True(t, ok)
+}
+
+func (ts *BTreeLeafTestSuite) TestDelete() {
+	t := ts.T()
+
+	layout := indexes.NewBTreeLeafPageLayout(records.Int64Field, 0)
+
+	searchVal := int64(33)
+	searchKey := scan.NewInt64Constant(searchVal)
+
+	firstRID := types.RID{
+		BlockNumber: 145,
+		Slot:        0,
+	}
+
+	maxRecs := (defaultTestBlockSize - leafPageDataOffset) / layout.SlotSize
+	testRecsCount := (maxRecs/2 - maxRecs/4)
+
+	testPageData := []btreeLeafTestsPageData{
+		{
+			SearchKey: scan.NewInt64Constant(searchVal),
+			Count:     int((maxRecs - testRecsCount) / 2),
+			FirstRID: types.RID{
+				BlockNumber: 235,
+				Slot:        350,
+			},
+		},
+		{
+			SearchKey: scan.NewInt64Constant(searchVal),
+			Count:     int(testRecsCount),
+			FirstRID:  firstRID,
+		},
+		{
+			SearchKey: scan.NewInt64Constant(searchVal),
+			Count:     int(maxRecs - (maxRecs-testRecsCount)/2 - testRecsCount),
+			FirstRID: types.RID{
+				BlockNumber: 335,
+				Slot:        150,
+			},
+		},
+	}
+
+	sut, page, _, fm := ts.newSUT(layout, searchKey, testPageData)
+	defer sut.Close()
+	defer func() {
+		require.NoError(t, fm.Close())
+	}()
+
+	for i := 0; i < int(testRecsCount); i++ {
+		require.NoError(t, sut.ResetSlot())
+
+		ok, err := sut.Delete(types.RID{
+			BlockNumber: firstRID.BlockNumber + types.BlockID(i),
+			Slot:        firstRID.Slot + types.SlotID(i),
+		})
+		require.NoError(t, err)
+		require.True(t, ok)
+	}
+
+	recs, err := page.GetRecords()
+	require.NoError(t, err)
+	require.EqualValues(t, ts.testDataRecords(testPageData)-int(testRecsCount), recs)
+
+	require.NoError(t, sut.ResetSlot())
+
+	ok, err := sut.Next()
+	assert.NoError(t, err)
+	assert.True(t, ok)
 }
